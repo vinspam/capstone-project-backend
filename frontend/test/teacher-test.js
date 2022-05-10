e4f21cd93dd697a8778efd2cdf5ba89bc72e4348
vinspam <brandonconnor0314@gmail.com>
2022-05-11 00:12:40 +0530

    updated frontend testing

---
+++ frontend/test/teacher-test.js
@@ -0,0 +1,39 @@
+import { Selector } from 'testcafe';
+
+fixture`Testing user interface`
+    .page`http://localhost:4200/`;
+
+
+test('Testing add teachers', async t => {
+    await t.navigateTo("/addTeacher");
+    await t.typeText("#teacher-id", "123456");
+    await t.typeText("#teacher-name", "Mohan Perera");
+    await t.typeText("#teacher-age", "45");
+    await t.click("#teacher-add");
+
+    await t.navigateTo("/");
+
+    const table = Selector('#teacher-table')
+    const rowCount = await table.find('tr').count;
+
+    let tdText = await table.find('tr').nth(rowCount - 1).innerText;
+    await t.expect(tdText).contains("Mohan Perera");
+});
+
+test('Testing delete teachers', async t => {
+    await t.navigateTo("/addTeacher");
+    await t.typeText("#teacher-id", "300000");
+    await t.typeText("#teacher-name", "Hasitha Fernando");
+    await t.typeText("#teacher-age", "45");
+    await t.click("#teacher-add");
+
+    await t.navigateTo("/");
+
+    await t.click("#teacher-delete-300000");
+
+    const table = Selector('#teacher-table')
+    const rowCount = await table.find('tr').count;
+
+    let tdText = await table.find('tr').nth(rowCount - 1).innerText;
+    await t.expect(tdText).notContains("Hasitha Fernando");
+});
